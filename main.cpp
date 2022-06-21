c31d495bf87cee38f890ccaaf31a0120f872d1ff
Rustam <Rustam2202@yandex.ru>
2022-06-21 20:33:49 +0300

    save

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "json_reader.h"
+#include "log_duration.h"
 #include "map_renderer.h"
 #include "request_handler.h"
 
@@ -8,11 +9,14 @@
 int main() {
 	using namespace std;
 
-	fstream input_file("test_12_1_input.json"s); // s10_final_opentest_1
+	fstream input_file("test_12_2_input.json"s); // s10_final_opentest_1
 	fstream output_file;
 	istream& strm(input_file);
 	output_file.open("test_12_1_output.json");
-	ReadJSON(strm, output_file);
+	{
+		LOG_DURATION("speed");
+		ReadJSON(strm, output_file); 
+	}
 	output_file.close();
 }
 
