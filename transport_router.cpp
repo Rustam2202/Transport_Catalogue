80f57416e12742e73b2ef95d08f2aad47f732d45
Rustam2202 <91486552+Rustam2202@users.noreply.github.com>
2022-06-23 12:17:37 +0300

    save

--- transport_router.cpp
+++ transport_router.cpp
@@ -8,7 +8,7 @@ TransportGraph::TransportGraph(TransportCatalogue& catalogue, int wait_time, int
 {
 	size_t i = 0;
 	for (const auto& stop : catalogue.GetStops()) {
-		stops_name_and_id_[stop.stop_name] = i;
+		stops_name_to_id_[stop.stop_name] = i;
 		i++;
 	}
 	for (const Bus& bus : catalogue.GetBuses()) {
@@ -16,13 +16,14 @@ TransportGraph::TransportGraph(TransportCatalogue& catalogue, int wait_time, int
 			double route_length = 0;
 			for (size_t j = k + 1; j < bus.stops_vector.size(); ++j) {
 				graph::Edge<WaitAndMovement> temp;
-				temp.from = stops_name_and_id_.at(bus.stops_vector[k]->stop_name);
-				temp.to = stops_name_and_id_.at(bus.stops_vector[j]->stop_name);
+				temp.from = stops_name_to_id_.at(bus.stops_vector[k]->stop_name);
+				temp.to = stops_name_to_id_.at(bus.stops_vector[j]->stop_name);
 				uint64_t dist = catalogue.GetDistanceBetweenStops(bus.stops_vector[j - 1]->stop_name, bus.stops_vector[j]->stop_name);
 				route_length += dist;
 				temp.weight.movement = CalculateMoveWeight(route_length);
 				temp.weight.wait = bus_wait_time_;
 				graph_.AddEdge(temp);
+				id_to_bus_name_.push_back(bus.bus_name);
 			}
 		}
 		if (!bus.is_ring) {
@@ -30,13 +31,14 @@ TransportGraph::TransportGraph(TransportCatalogue& catalogue, int wait_time, int
 				double route_length = 0;
 				for (int j = k - 1; j > -1; --j) {
 					graph::Edge<WaitAndMovement> temp;
-					temp.from = stops_name_and_id_.at(bus.stops_vector[k]->stop_name);
-					temp.to = stops_name_and_id_.at(bus.stops_vector[j]->stop_name);
+					temp.from = stops_name_to_id_.at(bus.stops_vector[k]->stop_name);
+					temp.to = stops_name_to_id_.at(bus.stops_vector[j]->stop_name);
 					uint64_t dist = catalogue.GetDistanceBetweenStops(bus.stops_vector[j + 1]->stop_name, bus.stops_vector[j]->stop_name);
 					route_length += dist;
 					temp.weight.movement = CalculateMoveWeight(route_length);
 					temp.weight.wait = bus_wait_time_;
 					graph_.AddEdge(temp);
+					id_to_bus_name_.push_back(bus.bus_name);
 				}
 			}
 		}
--- transport_router.h
+++ transport_router.h
@@ -30,12 +30,14 @@ inline WaitAndMovement operator+(const WaitAndMovement& lhs, const WaitAndMoveme
 class TransportGraph {
 	using StopIdInfo = std::unordered_map<std::string_view, int, Hasher>;
 	using GraphType = graph::DirectedWeightedGraph<WaitAndMovement>;
+	using EdgeIdToBusName = std::vector<std::string_view>; // index = edgeId, value = bus_name
 
 public:
 	TransportGraph(TransportCatalogue& catalogue, int wait_time, int velocity);
 
 	const GraphType& GetGraph() { return graph_; }
-	size_t GetIdOfStopName(std::string_view name) { return stops_name_and_id_.at(name); }
+	size_t GetIdOfStopName(std::string_view name) { return stops_name_to_id_.at(name); }
+	std::string_view GetBusNameById(size_t id) { return id_to_bus_name_.at(id); }
 
 private:
 	double CalculateMoveWeight(uint64_t dist) {
@@ -44,8 +46,8 @@ private:
 
 	int bus_wait_time_ = 0;
 	int bus_velocity_ = 0;
-	StopIdInfo stops_name_and_id_;
-	WaitAndMovement weight_;
+	StopIdInfo stops_name_to_id_;
+	EdgeIdToBusName id_to_bus_name_;
 	GraphType graph_;
 };
 
@@ -53,12 +55,19 @@ class TransportRoter :public TransportGraph {
 public:
 	TransportRoter(TransportCatalogue& catalogue, int wait_time, int velocity) :
 		TransportGraph(catalogue, wait_time, velocity),
-		router_(GetGraph()) {}
+		stops_(catalogue.GetStops()),
+		router_(GetGraph())
+	{}
 
-	auto BuildRoute(std::string_view from, std::string_view to) {
-		return router_.BuildRoute(GetIdOfStopName(from), GetIdOfStopName(to));
+	void BuildRoute(std::string_view from, std::string_view to) {
+		auto result = router_.BuildRoute(GetIdOfStopName(from), GetIdOfStopName(to));
+		for (size_t edge_id : result.value().edges) {
+			auto bus_name = GetBusNameById(edge_id);
+			auto& stop_name = stops_.at(GetGraph().GetEdge(edge_id).from);
+		}
 	}
 
 private:
+	const std::deque<Stop>& stops_;
 	graph::Router<WaitAndMovement> router_;
 };
